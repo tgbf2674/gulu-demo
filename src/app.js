41748ce126a02f0984a9fe4c37ca87f036fa1171
tgbf2674 <tgbf2020@163.com>
2020-12-10 15:49:20 +0800

    创建button group

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
 import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group'
+Vue.component('g-button-group',ButtonGroup);
 Vue.component('g-button',Button);
 Vue.component('g-icon',Icon);
 new Vue({
     el: '#app',
     data:{
         loading1: false,
-        loading2: false,
+        loading2: true,
         loading3: false
 
     }
