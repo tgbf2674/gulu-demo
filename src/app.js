44b295dff06fcf910e181e9e643837f4456f9e0a
tgbf2674 <tgbf2020@163.com>
2020-12-14 16:15:09 +0800

    添加input.vue 及其样式

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Button from './button.vue'
 import Icon from './icon.vue'
-import ButtonGroup from './button-group'
+import ButtonGroup from './button-group.vue'
+import Input from './input.vue'
 
 Vue.component('g-button-group', ButtonGroup);
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
+Vue.component('g-input',Input)
 new Vue({
     el: '#app',
     data: {
@@ -15,90 +17,3 @@ new Vue({
     }
 });
 
-//单元测试
-import chai from 'chai'
-import spise from 'chai-spies'
-chai.use(spise);
-const expect = chai.expect;
-{
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    });
-    vm.$mount();
-    let useElement = vm.$el.querySelector('use');
-    console.log(useElement);
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings');
-    vm.$el.remove();
-    vm.$destroy();
-}
-
-{
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings',
-            loading: true
-        }
-    });
-    vm.$mount();
-    let useElement = vm.$el.querySelector('use');
-    console.log(useElement);
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading');
-    vm.$el.remove();
-    vm.$destroy();
-}
-{
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    });
-    vm.$mount(div);
-    let svg = vm.$el.querySelector('svg');
-    let {order} = window.getComputedStyle(svg);
-    expect(order).to.eq('1');
-    vm.$el.remove();
-    vm.$destroy();
-}
-
-{
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings',
-            iconPosition: 'right'
-        }
-    });
-    vm.$mount(div);
-    let svg = vm.$el.querySelector('svg');
-    let {order} = window.getComputedStyle(svg);
-    expect(order).to.eq('2');
-    vm.$el.remove();
-    vm.$destroy();
-}
-{
-    //mock
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings',
-            iconPosition: 'right'
-        }
-    });
-    vm.$mount();
-    let spy = chai.spy(function () {
-
-    });
-    vm.$on('click',spy);
-    let button = vm.$el;
-    button.click();
-    expect(spy).to.have.been.called()
-}
--- src/button.vue
+++ src/button.vue
@@ -13,6 +13,7 @@
 <script>
     import Icon from './icon'
     export default {
+        name: 'GuluButton',
         // props: ['icon','iconPosition']
         components: {
           'g-icon': Icon
