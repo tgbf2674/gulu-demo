987da7027744bdebd49a35392e6ed6e918f7e542
tgbf2674 <tgbf2020@163.com>
2020-12-17 17:07:31 +0800

    默认就是phone样式

--- src/col.vue
+++ src/col.vue
@@ -25,10 +25,7 @@
             offset: {
                 type: [Number, String]
             },
-            phone :{
-                type: Object,
-                validator
-            },
+
             ipad: {
                 type: Object,
                 validator
@@ -53,10 +50,8 @@
         },
         computed:{
             colClass(){
-                let {span,offset,phone,ipad,narrowPc,pc,widePc} =this
-                let phoneClass = []
+                let {span,offset,ipad,narrowPc,pc,widePc} =this
                 return [span&& `col-${span}`, offset && `offset-${offset}`,
-                    ...(phone && [`col-phone-${phone.span}`]),
                     ...(ipad && [`col-ipad-${ipad.span}`]),
                     ...(narrowPc && [`col-narrowPc-${narrowPc.span}`]),
                     ...(pc && [`col-pc-${pc.span}`]),
@@ -89,20 +84,7 @@
                 margin-left: ($n /24) * 100%;
             }
         }
-        @media (max-width: 576px) {
-            $class-prefix: col-phone-;
-            @for $n from 1 through 24 {
-                &.#{$class-prefix}#{$n} {
-                    width: ($n /24) * 100%;
-                }
-            }
-            $class-prefix: offset-phone-;
-            @for $n from 1 through 24 {
-                &.#{$class-prefix}#{$n} {
-                    margin-left: ($n /24) * 100%;
-                }
-            }
-        }
+
         @media (min-width:577px) and (max-width: 768px) {
             $class-prefix: col-ipad-;
             @for $n from 1 through 24 {
