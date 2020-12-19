53c82e411e73fe2f77c78c660b4a186920b1340c
tgbf2674 <tgbf2020@163.com>
2020-12-19 20:37:04 +0800

    添加简单动画 添加toast 和plugin

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ import Footer from './footer.vue'
 import Header from './header.vue'
 import Sider from './sider.vue'
 import Content from './content.vue'
-
+import Toast from './toast.vue'
+import plugin from "./plugin"
 Vue.component('g-button-group', ButtonGroup);
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
@@ -22,6 +23,8 @@ Vue.component('g-header',Header)
 Vue.component('g-footer',Footer)
 Vue.component('g-sider',Sider)
 Vue.component('g-content',Content)
+Vue.component('g-toast',Toast)
+Vue.use(plugin)
 new Vue({
     el: '#app',
     data: {
@@ -31,9 +34,12 @@ new Vue({
         message: 'hi'
     },
 
+    created(){
+    },
     methods:{
-        inputChange(e){
-            console.log(e)
+        showToast(){
+            this.$toast('我是message')
+
         }
     }
 });
