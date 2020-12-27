c881a12c7109af5981869eeca5b4a5e18b7b50ce
tgbf2674 <tgbf2020@163.com>
2020-12-28 02:07:38 +0800

    添加首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,12 @@ module.exports = {
     base: '/gulu-demo/',
     title: 'Gulu UI',
     description: '一个好用的UI框架',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ],
     themeConfig: {
         nav: [
             {text: '主页', link: '/'},
-            {text: '文档', link: '/guide/'},
-            {text: '交流', link: '/http://google.com'},
         ],
         sidebar: [
             {
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,12 @@
-# Gulu UI官网
-
-Gulu UI 是一个UI框架，提供了...等常用组件，适合移动端和PC使用。
-
-
+---
+home: true
+actionText: 快速上手 →
+actionLink: /components/button/
+features:
+- title: 简洁实用
+  details: 模仿Element UI,Ant Design 的实用库
+- title: Vue
+  details: 基于Vue开发的UI组件库
+- title: 未完待续
+  details: 目前只有为数不多的组件，后期会继续扩展
+---
